1e77c723dff7ebd2db92e0773185fd48f4deb4ec
mb1 <mbarolak@gmail.com>
2013-08-10 17:53:18 -0400

    The HTTP POST for /eval is now working.

--- src/contest_api.ml
+++ src/contest_api.ml
@@ -83,18 +83,30 @@ let send_post post_url post_body =
 
 let add_to_operator_set op_string operator_set =
   match op_string with
-      "not"   -> {op1 = Not :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "shl1"  -> {op1 = Shl1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "shr1"  -> {op1 = Shr1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "shr4"  -> {op1 = Shr4 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "shr16" -> {op1 = Shr16 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "and"   -> {op1 = operator_set.op1; op2 = And :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "or"    -> {op1 = operator_set.op1; op2 = Or :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "xor"   -> {op1 = operator_set.op1; op2 = Xor :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "plus"  -> {op1 = operator_set.op1; op2 = Plus :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "if0"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = true; fold = operator_set.fold; tfold = operator_set.tfold}
-    | "fold"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = true; tfold = operator_set.tfold}
-    | "tfold"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = true}
+      "not"   -> {op1 = Not :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shl1"  -> {op1 = Shl1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr1"  -> {op1 = Shr1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr4"  -> {op1 = Shr4 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr16" -> {op1 = Shr16 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "and"   -> {op1 = operator_set.op1; op2 = And :: operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "or"    -> {op1 = operator_set.op1; op2 = Or :: operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "xor"   -> {op1 = operator_set.op1; op2 = Xor :: operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "plus"  -> {op1 = operator_set.op1; op2 = Plus :: operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "if0"   -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = true;
+                  fold = operator_set.fold; tfold = operator_set.tfold}
+    | "fold"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = true; tfold = operator_set.tfold}
+    | "tfold" -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0;
+                  fold = operator_set.fold; tfold = true}
     | _       -> invalid_arg ("'" ^ op_string ^ "' is not a known operator.")
 ;;
 
@@ -107,33 +119,41 @@ let rec parse_operator_set op_list =
 
 type problem_property = ProblemId of string | ProblemSize of int | ProblemOperators of operator_set | None;;
 
-let parse_problem_property property =
-  match property with
-      ("id", `String id) -> ProblemId id
-    | ("size", `Int size) -> ProblemSize size
-    | ("operators", `List operators) -> ProblemOperators (parse_operator_set operators)
-    | _ -> None
-;;
-
-let parse_problem_property_list prop_list =
-  let rec iter specs property =
-    match (parse_problem_property property) with
-        ProblemId id -> {id = id; size = specs.size; operators = specs.operators}
-      | ProblemSize size -> {id = specs.id; size = size; operators = specs.operators}
-      | ProblemOperators ops -> {id = specs.id; size = specs.size; operators = ops}
-      | None -> specs in
-  List.fold_left iter {id = "-1"; size = -1; operators = empty_operator_set} prop_list
-;;
-
 let parse_problem problem =
+  let parse_problem_property property =
+    match property with
+        ("id", `String id) -> ProblemId id
+      | ("size", `Int size) -> ProblemSize size
+      | ("operators", `List operators) -> ProblemOperators (parse_operator_set operators)
+      | _ -> None in
+  let parse_problem_property_list prop_list =
+    let rec iter specs property =
+      match (parse_problem_property property) with
+          ProblemId id -> {id = id; size = specs.size; operators = specs.operators}
+        | ProblemSize size -> {id = specs.id; size = size; operators = specs.operators}
+        | ProblemOperators ops -> {id = specs.id; size = specs.size; operators = ops}
+        | None -> specs in
+    List.fold_left iter {id = "-1"; size = -1; operators = empty_operator_set} prop_list in
   match from_string(problem) with
       `Assoc problem_spec -> parse_problem_property_list problem_spec
     | _ -> invalid_arg "Problem definition is not properly formatted."
 ;;
 
+let eval_post_body problem_id inputs =
+  "{"
+  ^ "\"id\": \"" ^ problem_id ^ "\", "
+  ^ "\"arguments\": [" ^ (join_with_commas (Array.to_list (Array.map (fun x -> "\"" ^ x ^ "\"") inputs))) ^ "]}"
+;;
+
 let guess_post_body problem_id program =
+  let rec join_with_commas str_list =
+    match str_list with
+        [] -> ""
+      | x::[] -> x
+      | x::y::z -> x ^ ", " ^ (join_with_commas (y::z)) in
   "{\"id\": \"" ^ problem_id ^ "\", \"program\": \"" ^ (program_to_string program) ^ "\"}"
 ;;
+
 
 (* The interesting functions *)
 
@@ -142,8 +162,8 @@ let get_training_problem size =
 ;;
 
 (* TODO(mb1): Actually write this function. *)
-let evaluate problem inputs =
-  send_post eval_post_url 
+let evaluate problem_id inputs =
+  send_post eval_post_url (eval_post_body problem_id inputs)
 ;;
 
 let guess problem_id program =
