7b77cb2a5f3141919a3301e71b80dc20363958bd
mb1 <mbarolak@gmail.com>
2013-08-09 21:47:39 -0400

    Adding some basic code for interacting with the contest API.

---
+++ src/contest_api.ml
@@ -0,0 +1,16 @@
+open Http_client;;
+open String;;
+
+let train_post_url = "http://icfpc2013.cloudapp.net/train?auth=0191yxaUHzX7C1if61Js0utpeBAUYTCAAlmgdvbAvpsH1H";;
+
+let problem_size n = "{\"size\": " ^ (string_of_int n) ^ "}";;
+
+let send_post post_url post_body =
+	let pipeline = new pipeline in
+	let post_op = new Http_client.post_raw post_url post_body in
+		pipeline#add(post_op);
+		pipeline#run();
+		post_op#get_resp_body();;
+
+let get_training_problem size =
+	send_post train_post_url (problem_size size)
