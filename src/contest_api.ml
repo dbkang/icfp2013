def563ba6c5f7f6a192ee8e80a99465b26ea0f44
mb1 <mbarolak@gmail.com>
2013-08-09 22:03:48 -0400

    Fixed the tabs, added a usage comment.

--- src/contest_api.ml
+++ src/contest_api.ml
@@ -1,3 +1,12 @@
+(* I've only been able to run this in an interactive shell so 
+   far.  To load this in the interactive shell, run these
+   commands:
+
+       #use "topfind";;
+       #require "netclient";;
+       #use "src/contest_api.ml";;
+*)
+
 open Http_client;;
 open String;;
 
@@ -6,11 +15,11 @@ let train_post_url = "http://icfpc2013.cloudapp.net/train?auth=0191yxaUHzX7C1if6
 let problem_size n = "{\"size\": " ^ (string_of_int n) ^ "}";;
 
 let send_post post_url post_body =
-	let pipeline = new pipeline in
-	let post_op = new Http_client.post_raw post_url post_body in
-		pipeline#add(post_op);
-		pipeline#run();
-		post_op#get_resp_body();;
+  let pipeline = new pipeline in
+  let post_op = new Http_client.post_raw post_url post_body in
+    pipeline#add(post_op);
+    pipeline#run();
+    post_op#get_resp_body();;
 
 let get_training_problem size =
-	send_post train_post_url (problem_size size)
+  send_post train_post_url (problem_size size)
