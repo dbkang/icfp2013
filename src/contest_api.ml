a0c736f2a230f66c139ebf3a44d0866f1ddb5f9b
mb1 <mbarolak@gmail.com>
2013-08-10 16:58:02 -0400

    Changed the format of the problem data type and updated the code that parses problems to that format

--- src/contest_api.ml
+++ src/contest_api.ml
@@ -18,13 +18,16 @@ open Yojson.Safe;;
 
 type problem_id = string;;
 
-type oper = Oper1 of op1 | Oper2 of op2
+type oper = Oper1 of op1 | Oper2 of op2 | If0 | Fold | TFold;;
 
-type operator_set = {op1: op1; op2: op2; if0: bool; fold: bool; tfold: bool};;
+type operator_set = {op1: op1 list; op2: op2 list; if0: bool; fold: bool; tfold: bool};;
 
-type problem = {id: problem_id; size: int; operators: oper list};;
+type problem = {id: problem_id; size: int; operators: operator_set};;
+
 
 (* Datatype helper functions *)
+
+let empty_operator_set = {op1 = []; op2 = []; if0 = false; fold = false; tfold = false};;
 
 let rec join_with_commas str_list =
   match str_list with
@@ -33,25 +36,25 @@ let rec join_with_commas str_list =
     | x::y::z -> x ^ ", " ^ (join_with_commas (y::z))
 ;;
 
-let rec add_most int_list =
-  match int_list with
-      [] -> []
-    | x::[] ->[x]
-    | x::y::z -> add_most (y::z);;
-
 let operator_set_to_string op_set =
+  join_with_commas (List.flatten [
+    (List.map op1_to_string op_set.op1);
+    (List.map op2_to_string op_set.op2);
+    (match op_set.if0 with true -> ["if0"] | false -> []);
+    (match op_set.fold with true -> ["fold"] | false -> []);
+    (match op_set.tfold with true -> ["tfold"] | false -> []);
+  ])
+;;
 
-
+(*
 let problem_to_string problem =
-  let rec 
-  let operator_list_to_string operator_list =
-
   "{\n"
-  ^ "  id: " ^ problem.problem_id ^ "\n"
+  ^ "  id: " ^ problem.id ^ "\n"
   ^ "  size: " ^ string_of_int(problem.size) ^ "\n"
-  ^ "  operators: " ^ (operator_list_to_string problem.operators) ^ "\n"
+  ^ "  operators: " ^ (operator_set_to_string problem.operators) ^ "\n"
   ^ "}\n"
 ;;
+*)
 
 
 (* Contest API constants*)
@@ -100,14 +103,39 @@ let rec parse_operator_list op_list =
       [] -> []
     | (`String op)::tail -> (parse_operator_string op)::(parse_operator_list tail)
     | _ -> invalid_arg "The problem definition's operator list contains an item that does not match (`String _)."
+;;
 
-type problem_property = ProblemId of string | ProblemSize of int | ProblemOperators of oper list | None;;
+let add_to_operator_set op_string operator_set =
+  match op_string with
+      "not"   -> {op1 = Not :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shl1"  -> {op1 = Shl1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr1"  -> {op1 = Shr1 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr4"  -> {op1 = Shr4 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "shr16" -> {op1 = Shr16 :: operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "and"   -> {op1 = operator_set.op1; op2 = And :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "or"    -> {op1 = operator_set.op1; op2 = Or :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "xor"   -> {op1 = operator_set.op1; op2 = Xor :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "plus"  -> {op1 = operator_set.op1; op2 = Plus :: operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "if0"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = true; fold = operator_set.fold; tfold = operator_set.tfold}
+    | "fold"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = true; tfold = operator_set.tfold}
+    | "tfold"  -> {op1 = operator_set.op1; op2 = operator_set.op2; if0 = operator_set.if0; fold = operator_set.fold; tfold = true}
+    | _       -> invalid_arg ("'" ^ op_string ^ "' is not a known operator.")
+;;
+
+let rec parse_operator_set op_list =
+  match op_list with
+      [] -> empty_operator_set
+    | (`String op)::tail -> (add_to_operator_set op (parse_operator_set tail))
+    | _ -> invalid_arg "The problem definition's operator list contains an item that does not match (`String _)."
+;;
+
+type problem_property = ProblemId of string | ProblemSize of int | ProblemOperators of operator_set | None;;
 
 let parse_problem_property property =
   match property with
       ("id", `String id) -> ProblemId id
     | ("size", `Int size) -> ProblemSize size
-    | ("operators", `List operators) -> ProblemOperators (parse_operator_list operators)
+    | ("operators", `List operators) -> ProblemOperators (parse_operator_set operators)
     | _ -> None
 ;;
 
@@ -118,7 +146,7 @@ let parse_problem_property_list prop_list =
       | ProblemSize size -> {id = specs.id; size = size; operators = specs.operators}
       | ProblemOperators ops -> {id = specs.id; size = specs.size; operators = ops}
       | None -> specs in
-  List.fold_left iter {id = "fake"; size = 0; operators = []} prop_list
+  List.fold_left iter {id = "-1"; size = -1; operators = empty_operator_set} prop_list
 ;;
 
 let parse_problem problem =
